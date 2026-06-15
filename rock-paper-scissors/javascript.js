170847bd9595229cc15e3397fbaa90d44481cee4
ola-muaddel <o0ola.ol0100@gmail.com>
2026-06-15 11:16:37 +0300

    Add getHumanChoice function : This function will return the user choice

--- rock-paper-scissors/javascript.js
+++ rock-paper-scissors/javascript.js
@@ -1,3 +1,8 @@
+/* date : 2026-06-15 */
+
+/* (1) we can use console.log(probability) to check the 
+ probability and see if the result of console.log(getComputerChoice())
+ match the choice we put based on that probability */
 function getComputerChoice() {
     let probability = Math.random();
     let choice;
@@ -13,3 +18,13 @@ function getComputerChoice() {
     return choice;
 
 }
+
+/*(2) this function will get user input using prompt 
+and return the user input in lower case I can check it
+with console.log(getComputerChoice()) */
+function getHumanChoice() {
+    let userInput = prompt("enter you choice : ROCK | Paper | Scissors");
+    return userInput.toLowerCase();
+}
+
+
